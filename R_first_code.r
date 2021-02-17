c154e5f6564508f79d1733552c9ee1414cea921f
nashmill-environmental <74115171+nashmill-environmental@users.noreply.github.com>
2021-02-17 12:44:47 +0300

    Create R_first_code.r

---
+++ R_first_code.r
@@ -0,0 +1,33 @@
+# write whaever you want here! 
+
+# this is an array
+primates <- c(3, 5, 9, 15, 40)
+primates 
+
+# new set:
+crabs <- c(100, 70, 30, 10,5)
+crabs
+
+# first plot in R
+plot(primates, crabs)
+
+plot(primates, crabs, col="red")
+
+plot(primates, crabs, col="red", pch=19)
+
+plot(primates, crabs, col="red", pch=19, cex=2)
+
+plot(primates, crabs, col="red", pch=19, cex=6)
+
+plot(primates, crabs, col="red", pch=19, cex=6, main="My first ecological gragh in R!")
+
+# create a dataframe
+ecoset <- data.frame(primates, crabs)
+ecoset
+
+# ecological numbers
+# mean number of individualsm per site
+(3+5+9+15+40)/5
+
+# summary of the dataframe
+summary(ecoset) # laurel: Ecosets
