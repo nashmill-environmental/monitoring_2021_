a609580eb191c5ac938e91244b2f47a351e1151a
nashmill-environmental <74115171+nashmill-environmental@users.noreply.github.com>
2021-02-21 15:40:22 +0300

    Update R_code_point_pattern_analysis.r

--- R_code_point_pattern_analysis.r
+++ R_code_point_pattern_analysis.r
@@ -31,13 +31,13 @@ attach(covid)
 # x, y, ranges
 covid_planar <- ppp(lon, lat, c(-180, 180), c(-90, 90))
 
-density_map <- dencity(covid_planar)
+density_map <- density(covid_planar)
 
 plot(density_map)
 points(covid_planar)
 
 # Exercise: change the colour of the map
-cl <- colorRampPLatte (c('blue', 'yellow', 'orang', 'red', 'magenta'))(100) #
+cl <- colorRampPalette (c('blue','yellow','orange','red','magenta'))(100) #
 plot(density_map, col = cl)
 points(covid_planar)
 
@@ -48,15 +48,43 @@ points(covid_planar)
 # sudo apt-get update
 # sudo apt-get install libgdal-dev libproj-dev
 
-install.packages("rgdal")
+install.packages("rgdal") 
+
+library(rgdal)
 
 
+coastlines <- readOGR("ne_10m_coastline.shp")
+
+# cl <- colorRampPalette (c('pink','yellow','orange','red'))(100) #
+plot(density_map, col = cl)
+points(covid_planar, pch = 19, cex = 0.5)
+plot(coastlines, add = TRUE)
+
+png("figure1.png")
+cl <- colorRampPalette (c('pink','yellow','orange','red'))(100) #
+plot(density_map, col = cl)
+points(covid_planar, pch = 19, cex = 0.5)
+plot(coastlines, add = TRUE)
+dev.off()
+
+pdf("figure1.pdf")
+cl <- colorRampPalette (c('pink','yellow','orange','red'))(100) #
+plot(density_map, col = cl)
+points(covid_planar, pch = 19, cex = 0.5)
+plot(coastlines, add = TRUE)
+dev.off()
+
+head(covid)
 
 
+###### interpolate case data
 
+marks(covid_planar) <- cases
+cases_map <- Smooth(covid_planar)
 
-
-
+plot(cases_map, col = cl)
+points(covid_planar)
+plot(coastlines, add = TRUE)
 
 
 
