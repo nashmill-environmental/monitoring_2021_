c157fbb7034a207ac180b453c5d10e437f7ad584
nashmill-environmental <74115171+nashmill-environmental@users.noreply.github.com>
2021-02-23 00:54:49 +0300

    Create R_code_exam_simulation.r

---
+++ R_code_exam_simulation.r
@@ -0,0 +1,24 @@
+# R code for the exam! !
+
+# nc data: we need library
+# https://cran.r-project.org/web/packages/ncdf4/index.html
+
+install.packages("ncdf4")
+library(ncdf4)
+library(raster)
+
+srtwed("c:/lab/")
+
+tjan <- raster("c_gls_LST_202101181300_GLOBE_GEO_V1.2.1")
+plot(tjan)
+
+# Exercise: change the colorramppalette
+cltjan <- colorRamp Palette(c('blue', 'yellow', 'red'))(100) #
+plot(tjan, col=cltjan
+
+toct <- raster("")
+plot(toct, col=cltjan)
+
+dift <- tjan - toct
+cldif <- colorRampPalette(c('blue', 'white', 'red'))(100) #
+plot(dift, col=cldif)
