fb57a9af94f1347fe9a6a7be76514ca70f1e9ab3
nashmill-environmental <74115171+nashmill-environmental@users.noreply.github.com>
2021-02-23 19:07:30 +0300

    Update R_code_energy.r

--- R_code_energy.r
+++ R_code_energy.r
@@ -37,3 +37,85 @@ copNDVI_Italy <- crop(copNDVI, ext)
 plot(copNDVI_Italy, col=clymax)
 
 
+
+
+########### DAY 2 
+# https://earthobservatory.nasa.gov/
+
+# Deforestation example
+library(raster)
+library(RStoolbox)
+
+setwd("C:/lab/")
+defor1 <- brick("defor1_.jpg") #defor1 <- brick("defort1_.png") if errors come out: defort1_.png.jpg
+
+plotRGB(defor1, r=1, g=2, b=3, stretch="Lin")
+ 
+# Exercise: import the defor2_.jpg image
+defor2 <- brick("defor2_.jpg") #defor2 <- brick("defort2_.png") if errors come out: defort2_.png.jpg 
+plotRGB(defor2, r=1, g=2, b=3, stretch="Lin")
+
+# Exercise: put images one beside the other with par()
+per(mfrow=c(2,1))
+plotRGB(defor1, r=1, g=2, b=3, stretch="Lin")
+plotRGB(defor2, r=1, g=2, b=3, stretch="Lin")
+
+
+# dvi for the first period
+dvi1 <- defor1$defor1_.1 - defor1$defor1_.2
+
+# dev.off()
+plot(dvi1)
+
+cl <- colorRampPalette(c('darkblue', 'yellow', 'red', 'black'))(100) # specifying a color schme
+plot(devi1, col=cl)
+
+# devi2 after the cut!
+dvi1 <- defor2$defor2_.1 - defor2$defor2_.2
+cl <- colorRampPalette(c('darkblue', 'yellow', 'red', 'black'))(100) # specifying a color schme
+plot(devi2, col=cl)
+
+# Exercise: put images one beside the other with par()
+per(mfrow=c(2,1))
+plot(devi1, col=cl, main="DVI before cut")
+plot(devi2, col=cl, main="DVI after cut")
+
+# defference biomass before cut and after cut
+difdvi <- dvi1 - dvi2
+dev.off()
+plot(difdvi)
+
+cldif <- colorRampPalette(c('blue', 'white', 'red'))(100)
+plot(difdvi, col=cldif, main="amount of energy lost!")
+
+
+hist(difdvi, col="red")
+
+
+
+######## final par!
+# defor1 
+# defor2
+# dvi1
+# dvi2 
+# difdvi
+# histogram
+
+par(mfrow=c(3,2))
+plotRGB(defor1, r=1, g=2, b=3, stretch="Lin")
+plotRGB(defor2, r=1, g=2, b=3, stretch="Lin")
+plot(devi1, col=cl, main="biomass before cut")
+plot(devi2, col=cl, main="biomass after cut")
+plot(difdvi, col=cldif, main="amount of energy lost!")
+hist(difdvi, col="red", "positive values mean change!")
+
+
+
+
+
+
+
+
+
+
+
